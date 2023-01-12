ebe747d763926390c434b2de94042e92d8d9f5f7
Dr. Katja Becker-Lindhorst <110469926+kbl98@users.noreply.github.com>
2023-01-12 14:47:08 +0100

    logout at head

--- js/login.js
+++ js/login.js
@@ -140,3 +140,14 @@ function set_new_password(){
     window.location.href="login.html";
 }
 }
+
+
+function logout(){
+    document.getElementById("popup-logout").classList.add("d-none");
+    window.location.href="login.html"
+}
+
+function showLogout(){
+    document.getElementById("popup-logout").classList.remove("d-none");
+}
+
