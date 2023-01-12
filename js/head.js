855c76f019b07dc937588c6f6710c706ac21642b
Dr. Katja Becker-Lindhorst <110469926+kbl98@users.noreply.github.com>
2023-01-12 19:36:46 +0100

    logout 2.0

--- js/head.js
+++ js/head.js
@@ -1,8 +1,15 @@
+/**function to go to login-window */
 function logout(){
     document.getElementById("popup-logout").classList.add("d-none");
     window.location.href="login.html"
 }
 
+/**function that toogles the display of logout-opportunity*/
 function showLogout(){
-    document.getElementById("popup-logout").classList.remove("d-none");
-}
+    let logout=document.getElementById("popup-logout");
+    if(logout.classList.contains("d-none")){
+    logout.classList.remove("d-none");}
+    else{
+        logout.classList.add("d-none");}
+    }
+
