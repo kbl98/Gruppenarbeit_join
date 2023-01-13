b73c0e4c5b2f8e38ed9edf528e0c7fb75a6b07db
Marcel Hofbauer <marcel.hof.0611@gmail.com>
2023-01-13 13:51:06 +0100

    added toggle open/close

--- js/contacts.js
+++ js/contacts.js
@@ -4,7 +4,14 @@ function openCloseDetails() {
     let openDetails = document.getElementById('openContact');
     let bgr = document.getElementById('contactBgr');
     let color = document.getElementById('contactNameColor');
-    openDetails.classList.remove('d-none');
-    bgr.style = 'background-color: #2A3647;';
-    color.style = 'color: white;';
+
+    if (openDetails.classList.contains('d-none')) {
+        openDetails.classList.remove('d-none');
+        bgr.style = 'background-color: #2A3647;';
+        color.style = 'color: white;';
+    } else {
+        openDetails.classList.add('d-none');
+        bgr.style = '';
+        color.style = '';
+    }
 }
