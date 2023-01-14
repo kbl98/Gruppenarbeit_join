c030cc2048bb357fb994bd8794121813014a9221
Dr. Katja Becker-Lindhorst <110469926+kbl98@users.noreply.github.com>
2023-01-15 00:57:49 +0100

    first part mobile at summary+login

--- js/head.js
+++ js/head.js
@@ -1,28 +1,27 @@
 /**function to go to login-window */
-function logout(){
-    document.getElementById("popup-logout").classList.add("d-none");
-    resetUser();
-    window.location.href="login.html"
+function logout() {
+  document.getElementById("popup-logout").classList.add("d-none");
+  resetUser();
+  window.location.href = "login.html";
 }
 
 /**function that toogles the display of logout-opportunity*/
-function showLogout(){
-    let logout=document.getElementById("popup-logout");
-    if(logout.classList.contains("d-none")){
-    logout.classList.remove("d-none");}
-    else{
-        logout.classList.add("d-none");}
-    }
-
-function resetUser(){
-     all_tasks=[];
- current_img="./assets/img/user_prototype.png";
- users=[];
- current_task=[];
- current_user=[];
- current_user=[];
- localStorage.removeItem("current_user");
+function showLogout() {
+  let logout = document.getElementById("popup-logout");
+  if (logout.classList.contains("d-none")) {
+    logout.classList.remove("d-none");
+  } else {
+    logout.classList.add("d-none");
+  }
 }
 
-
-
+/**function to clear variables at logout */
+function resetUser() {
+  all_tasks = [];
+  current_img = "./assets/img/user_prototype.png";
+  users = [];
+  current_task = [];
+  current_user = [];
+  current_user = [];
+  localStorage.removeItem("current_user");
+}
