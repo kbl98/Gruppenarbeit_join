01083e89cde1abeaf1bc1a3b540348cd1accb45c
Daniela Fehlmann <danielafehlmann@gmail.com>
2023-01-14 09:07:13 +0100

    add_task update

--- js/add_task.js
+++ js/add_task.js
@@ -5,6 +5,9 @@ let selectedContactNames = [];
 let invitedContacts = [];
 let selectedDate;
 let selectedPrio;
+let allSubtasks = [];
+
+
 //Title
 function addTitle() {
   let title = document.getElementById('titleInput');
@@ -24,6 +27,7 @@ function addDescription() {
 function addCategory(i) {
   let categoryName = document.getElementById('category');
   let categoryColor = document.getElementById('categoryColor');
+  categoryName.style.color='black';
   document.getElementById('category').innerHTML= document.getElementById('category'+i).innerHTML;
   document.getElementById('categoryColor').innerHTML= document.getElementById('imageCat'+i).innerHTML;
   document.getElementById('categoryImage').innerHTML = `<img src="assets/img/arrow_drop.svg">`;
@@ -37,15 +41,16 @@ function addCategory(i) {
 
 
 function addNewCategory() {
-  let category = document.getElementById('category');
-  category.contentEditable="true";
+  let categoryName = document.getElementById('category');
+  categoryName.contentEditable="true";
   openCloseCategories();
-  category.innerHTML='';
+  categoryName.innerHTML='';
+  categoryName.style.color='black';
   document.getElementById('categoryImage').innerHTML = `<div  onclick="notOpenCloseCategories(event)"><img src="assets/img/cross.svg" onclick="cancelNewCategory()"> <img src="assets/img/finish.svg" onclick="acceptNewCategory()"></div>`;
   document.getElementById('categoryColors').classList.remove('d-none');
   document.getElementById('selectField').removeAttribute('onclick');
   document.getElementById('categoryColor').innerHTML = '';
-  category.focus();
+  categoryName.focus();
 }
 
 
@@ -56,9 +61,10 @@ function changeColor(i) {
 
 
 function cancelNewCategory () {
-  let category = document.getElementById('category');
-  category.innerHTML='Select task category';
-  category.contentEditable="false";
+  let categoryName = document.getElementById('category');
+  categoryName.innerHTML='Select task category';
+  categoryName.contentEditable="false";
+  categoryName.style.color= '#dcdcdc';
   document.getElementById('categoryImage').innerHTML = `<img src="assets/img/arrow_drop.svg">`;
   document.getElementById('categoryColors').classList.add('d-none');
   document.getElementById('selectField').setAttribute('onclick','openCloseCategories()');
@@ -85,7 +91,7 @@ function acceptNewCategory (){
 
 function openCloseCategories() {
   if (document.getElementById('selectField').style.height == '192px') {
-    document.getElementById('selectField').style.height = '49px';
+    document.getElementById('selectField').style.height = '51px';
     document.getElementById('openedCategories').classList.add('d-none');
   } else {
   document.getElementById('selectField').style.height = '192px';
@@ -146,7 +152,7 @@ function acceptNewContact (){
 
 function openCloseContacts() {
   if (document.getElementById('selectFieldContact').style.height == '192px') {
-    document.getElementById('selectFieldContact').style.height = '49px';
+    document.getElementById('selectFieldContact').style.height = '51px';
     document.getElementById('openedContacts').classList.add('d-none');
   } else {
   document.getElementById('selectFieldContact').style.height = '192px';
@@ -190,6 +196,82 @@ function addPrio(i) {
       selectedPrio = prios[x];
     }
   }
+}
+
+
+//SUBTASKS
+function addNewSubtask(){
+  let newSubtaskInput = document.getElementById('newSubtaskInput');
+  allSubtasks.push(newSubtaskInput.value);
+  document.getElementById('newSubtasks').innerHTML = '';
+  for (let i = 0; i < allSubtasks.length; i++) {
+    const newSubtask = allSubtasks[i];
+  document.getElementById('newSubtasks').innerHTML += `
+  <div class="newSubtasks">
+    <img src="assets/img/subtask_rectangle.svg" class="paddingRight" id="checkbox${i}" onclick="checkmark(${i})"><span class="newSubtask">${newSubtask}</span>
+  </div>
+  `;
+
+}
+}
+//NICHT FERTIG
+function checkmark(i) {
+  document.getElementById('checkbox'+i).src='assets/img/subtask_ok.svg';
+}
+
+
+
+function clearTask() {
+  resetVariables();
+  resetContent();
+  }
+
+
+function resetVariables() {
+  selectedTitle = [];
+  selectedDescription = [];
+  selectedCategory = [];
+  selectedContactNames = [];
+  invitedContacts = [];
+  selectedDate = [];
+  selectedPrio = [];
+  allSubtasks = [];
+}
+
+
+function resetContent() {
+  document.getElementById('titleInput').value = '';
+  document.getElementById('descriptionTextarea').value = '';
+  cancelNewCategory();
+  closeCategories();
+  closeContacts();
+  document.getElementById('contactButton0').innerHTML='';
+  document.getElementById('contactButton1').innerHTML='';
+  document.getElementById('datepicker').value='';
+  resetPrioButtons();
+  document.getElementById('newSubtasks').innerHTML = '';
+  document.getElementById('newSubtaskInput').value = '';
+    
+}
+
+
+function closeCategories() {
+  document.getElementById('selectField').style.height = '51px';
+  document.getElementById('openedCategories').classList.add('d-none');
+}
+
+
+function closeContacts() {
+    document.getElementById('selectFieldContact').style.height = '51px';
+    document.getElementById('openedContacts').classList.add('d-none');
+}
+
+
+function resetPrioButtons() {
+  for (let i = 0; i < 3; i++) {
+    document.getElementById('prioButton'+i).style.backgroundColor = 'white';
+    document.getElementById('prioButton'+i).style.color = 'black';
+  };
 }
 
 
