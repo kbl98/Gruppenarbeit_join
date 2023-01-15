29a4ebfaf2ef5131594b4991fb50949ebdfbbdb8
Dr. Katja Becker-Lindhorst <110469926+kbl98@users.noreply.github.com>
2023-01-15 13:07:13 +0100

    summary.js set

--- js/summary.js
+++ js/summary.js
@@ -1,12 +1,14 @@
+let urgent_tasks=[];
+
 async function setMainHTML() {
   await getTasks();
   window.location.href = "main.html";
 }
 
+/**function that fetches tasks from backend and creates a Json */
 async function init() {
   await includeHTML();
-
-  setURL(
+setURL(
     "https://gruppe-430.developerakademie.net/smallest_backend_ever-master"
   );
   await downloadFromServer();
@@ -17,18 +19,6 @@ async function init() {
   console.log(all_tasks);
   renderSummary();
 }
-
-/**function that fetches tasks from backend and creates a Json */
-/*async function getTasks() {
-  setURL(
-    "http://gruppe-430.developerakademie.net/smallest_backend_ever-master"
-  );
-  let tasksAsText = await backend.getItem("tasks");
-  if(!tasksAsText){
-    console.log("Keine Tasks")
-  }
-  all_tasks = JSON.parse(tasksAsText);
-}*/
 
 /**function to include the template */
 async function includeHTML() {
@@ -56,7 +46,9 @@ function renderSummary() {
   let num_Feedback = document.getElementById("at-feedback");
   /*num_Feedback.innerHTML=countAtFeedback();*/
   let num_Urgent = document.getElementById("num-urgent");
-  num_Urgent = countUrgentTasks();
+  getAllUrgentTasks();
+  num_Urgent.innerHTML=urgent_tasks.length;
+  createUrgentBox()
 }
 
 function countTasksOnBoard() {
@@ -64,14 +56,44 @@ function countTasksOnBoard() {
   return onBoard;
 }
 
-function countUrgentTasks() {
-  let urgent = 0;
+
+function getAllUrgentTasks(){
   for (let i = 0; i < all_tasks.length; i++) {
     if (all_tasks[i]["prio"] == "urgent") {
-      urgent++;
+      urgent_tasks.push(all_tasks[i]);
+      
     }
   }
-  return urgent;
+}
+
+function createUrgentBox(){
+  let urgenttasks_container=document.getElementById("deadline-container-box");
+    urgenttasks_container.innerHTML="";
+  for(let i=0;i<urgent_tasks.length;i++){
+    let taskdate=constructDate(urgent_tasks[i]["date"]);
+    console.log(taskdate);
+    
+    urgenttasks_container.innerHTML+=generateUrgentHTML(i,taskdate);
+  }
+}
+
+function generateUrgentHTML(i,taskdate){
+  return `<div id="deadline-container${i}" class="deadline-container" onclick="getToBoard()">
+  <span id="deadline${i}" class="deadline">${taskdate}</span>
+  <p>Upcoming Deadline</p>
+</div>`
+}
+
+
+function constructDate(date){
+  console.log(date);
+  let [day,mo,ye]=date.split('/');
+    let d=new Date(+ye, +mo, + day);
+    let weekday=d.getDay();
+    let m=["January","February","March","April","May","June","July","August","September","October","November","December"]
+    let days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let taskdate=days[weekday-1]+", "+d.getDay()+"."+ m[d.getMonth()-1]+" "+d.getFullYear();
+    return taskdate;
 }
 
 function getToBoard() {
