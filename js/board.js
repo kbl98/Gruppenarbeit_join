f3bd16de0c9852e3f9b6069465c572195340cc2b
Marcel Hofbauer <marcel.hof.0611@gmail.com>
2023-01-20 21:30:16 +0100

    added board js

--- js/board.js
+++ js/board.js
@@ -1,20 +1,41 @@
 let loadedBoard = [];
+let loadedContacts = [];
+let currentDragElement;
 
 
 
 async function initBoard() {
     setURL('https://gruppe-430.developerakademie.net/smallest_backend_ever-master');
     await loadAllTaskForBoard();
+    await loadContactsFromBackend();
+    await getCurrentUserFromStorage();
+    renderBoard();
+    setUserImg();
+    checkForColor();
+}
+
+
+function dragStart(id) {
+    currentDragElement = id;
+}
+
+
+function drop(progress) {
+    loadedBoard[currentDragElement]['progress'] = progress;
     renderBoard();
 }
 
 
-//async function testSaveToBackend() {
-//    await downloadFromServer();
-//    let deinJSONAsText = JSON.stringify(deinJSON);
-//    await backend.setItem("all_tasks", deinJSONAsText);
-//}
+function allowDrop(ev) {
+    ev.preventDefault();
+}
 
+
+async function boardSaveToBackend() {
+    await downloadFromServer();
+    let JSONAsText = JSON.stringify(loadedBoard);
+    await backend.setItem("all_tasks", JSONAsText);
+}
 
 
 async function loadAllTaskForBoard() {
@@ -28,21 +49,29 @@ async function loadAllTaskForBoard() {
 }
 
 
+async function loadContactsFromBackend() {
+    await downloadFromServer();
+    loadedContacts = JSON.parse(backend.getItem("contacts"));
+    if (!loadedContacts) {
+        loadedContacts = [];
+    };
+
+    console.log('Alle Kontakte', loadedContacts);
+}
+
+
 function renderBoard() {
+    clearRender();
     for (let i = 0; i < loadedBoard.length; i++) {
         const currentProgress = loadedBoard[i]['progress'];
         if (currentProgress.includes('todo')) {
-            console.log('Todo true');
-            renderBoardTodo(currentProgress);
+            renderBoardFiltered(i, 'todo');
         } else if (currentProgress.includes('in Process')) {
-            //console.log('in Process true');
-            //renderBoardInProcess(currentProgress);
+            renderBoardFiltered(i, 'inProcess');
         } else if (currentProgress.includes('awaiting Feedback')) {
-            //console.log('awaiting Feedback true');
-            //renderBoardAwaitingFeedback(currentProgress);
+            renderBoardFiltered(i, 'awaitingFeedback');
         } else if (currentProgress.includes('done')) {
-            //console.log('done true');
-            //renderBoardDone(currentProgress);
+            renderBoardFiltered(i, 'done');
         } else {
             alert('Failed to load all Tasks - try to reload the page')
         }
@@ -50,21 +79,102 @@ function renderBoard() {
 }
 
 
-function renderBoardTodo(currentProgress) {
-    let todo = document.getElementById('contTodo');
+function clearRender() {
+    let todo = document.getElementById('todo');
+    let inProcess = document.getElementById('inProcess');
+    let awaitingFeedback = document.getElementById('awaitingFeedback');
+    let done = document.getElementById('done');
     todo.innerHTML = '';
-    for (let i = 0; i < loadedBoard.length; i++) {
-        let task = loadedBoard[i];
-        if (currentProgress.includes('todo')) {
-            let category = task['category'];
-            let titel = task['title'];
-            let description = task['description'];
-            let date = task['date'];
-            let priority = task['prio'];
-            let assignedTo = task['contactNames'];
-            //todo.innerHTML += renderBoardTemp(category, titel, description, date, priority, assignedTo);
+    inProcess.innerHTML = '';
+    awaitingFeedback.innerHTML = '';
+    done.innerHTML = '';
+}
+
+
+function renderBoardFiltered(index, id) {
+    let progress = document.getElementById(id);
+    let task = loadedBoard[index];
+    let { color, category, title, description, date, priority, assignedTo, assignedToLength } = getTaskInfos(task);
+    progress.innerHTML += renderBoardTemp(color, category, title, description, date, priority, assignedTo, id, index);
+    let assignedToId = document.getElementById(`assignedTo${id}${index}`);
+    assignedToId.innerHTML = '';
+    renderAssignedTo(assignedToLength, assignedTo, assignedToId);
+}
+
+
+
+function renderAssignedTo(assignedToLength, assignedTo, assignedToId) {
+    if (assignedToLength > 3) {
+        for (let x = 0; x < 2; x++) {
+            let currentName = assignedTo[x];
+            let bothFirstLetters = splitName(currentName);
+            let color = getContactColor(assignedToLength, assignedTo, x);
+            assignedToId.innerHTML += `<div style="background-color: ${color};" class="assigned">${bothFirstLetters}</div>`;
+        }
+        let numberOf = assignedToLength - 2;
+        assignedToId.innerHTML += `<div style="background-color: #000000;" class="assigned">+${numberOf}</div>`;
+    } else {
+        for (let x = 0; x < assignedToLength; x++) {
+            let currentName = assignedTo[x];
+            let bothFirstLetters = splitName(currentName);
+            let color = getContactColor(assignedToLength, assignedTo, x);
+            assignedToId.innerHTML += `<div style="background-color: ${color};" class="assigned">${bothFirstLetters}</div>`;
+
         }
     }
+}
+
+
+function getTaskLength(progress) {
+    let length = 0;
+    for (let i = 0; i < loadedBoard.length; i++) {
+        const currentProgress = loadedBoard[i]['progress'];
+        if (currentProgress.includes(progress)) {
+            length++;
+        }
+    }
+    return length;
+}
+
+
+function getContactColor(assignedToLength, assignedTo, j) {
+    for (let x = j; x < assignedToLength; x++) {
+        let currentName = assignedTo[x];
+        for (let i = 0; i < loadedContacts.length; i++) {
+            let contact = loadedContacts[i].name;
+            if (currentName.toLowerCase().includes("you")) {
+                currentName = current_user.username;
+            }
+            if (contact.includes(currentName)) {
+                return loadedContacts[i].color;
+            }
+        }
+    }
+}
+
+
+function getTaskInfos(task) {
+    let color = task['color']
+    let category = task['category'];
+    let title = task['title'];
+    let description = task['description'];
+    let date = task['date'];
+    let priority = task['prio'];
+    let assignedTo = task['contactNames'];
+    let assignedToLength = task['contactNames'].length;
+    return { color, category, title, description, date, priority, assignedTo, assignedToLength };
+}
+
+
+function splitName(fullName) {
+    // let fullName = "David Eisenberg";
+    let nameParts = fullName.split(" ");
+    let firstName = nameParts[0];
+    let lastName = nameParts[nameParts.length - 1];
+    let bothFirstLetters = firstName.charAt(0) + lastName.charAt(0);
+    return bothFirstLetters
+    // console.log(firstName); Output: "David"
+    // console.log(lastName); Output: "Eisenberg"
 }
 
 
@@ -80,26 +190,42 @@ function closeBoardTask() {
 }
 
 
-function renderBoardTemp(category, titel, description, date, priority, assignedTo) {
+async function getCurrentUserFromStorage() {
+    let currentUserAsText = localStorage.getItem("current_user");
+    if (!currentUserAsText) {
+        window.location.href = "login.html";
+    } else {
+        current_user = JSON.parse(currentUserAsText);
+    }
+}
+
+
+function checkForColor() {
+    for (let i = 0; i < loadedContacts.length; i++) {
+        let currentContact = loadedContacts[i];
+        if (!currentContact.color) {
+            let name = currentContact.name;
+            let email = currentContact.email;
+            let phone = currentContact.phone;
+            let randomColor = getRandomColor();
+            let newObjekt = { name: name, email: email, phone: phone, color: randomColor };
+            loadedContacts.splice(i, 1, newObjekt);
+        }
+    }
+}
+
+
+function renderBoardTemp(color, category, title, description, date, priority, assignedTo, progress, index) {
     return `
-    <div onclick="openBoardTask('${category}', '${titel}', '${description}', '${date}', '${priority}', '${assignedTo}')" class="todo-tasks">
-        <h2 style="background-color: #FF7A00;" class="task-head">Design</h2>
-        <span class="task-titel">Website redesign</span> <br><br>
-        <span class="task-description">Modify the contents of the main website...</span>
-        <div class="cont-progress">
-            <div class="progress">
-                <div style="width: 50% !important;" class="progress-bar">
-                </div>
-            </div>
-            <span>1/2 Done</span>
-        </div>
+    <div draggable="true" ondragstart="dragStart(${index})" onclick="openBoardTask('${color}', '${category}', '${title}', '${description}', '${date}', '${priority}', '${assignedTo}', '${progress}')" class="todo-tasks">
+        <h2 style="background-color: ${color};" class="task-head">${category}</h2>
+        <span class="task-titel">${title}</span> <br><br>
+        <span class="task-description">${description}</span>
         <div class="task-footer">
-            <div class="cont-assigned">
-                <div style="background-color: #0190E0;" class="assigned">SM</div>
-                <div style="background-color: #EE00D6;" class="assigned">MV</div>
-                <div style="background-color: #02CF2F;" class="assigned">EF</div>
+            <div id="assignedTo${progress}${index}" class="cont-assigned">
+
             </div>
-            <img class="board-priority" src="./assets/img/priority_low.png" alt="">
+            <img class="board-priority" src="./assets/img/priority_${priority}.png" alt="">
         </div>
     </div>
     `;
