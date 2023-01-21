7fe45287250cddeab3c9fc860d193003aeb82bfa
Dr. Katja Becker-Lindhorst <110469926+kbl98@users.noreply.github.com>
2023-01-21 18:06:17 +0100

    head color, form name

--- js/contacts.js
+++ js/contacts.js
@@ -311,7 +311,7 @@ function openNewContactTemp() {
                 <img class="create-contact-pp" src="./assets/img/contact_empty_pp.png" alt="">
 
                 <form onsubmit="createNewContact();return false" class="cont-create-contact-input">
-                    <input id="newContactNameValue" required placeholder="Name" type="text" />
+                    <input id="newContactNameValue" required placeholder="Vorname Nachname" pattern="[A-ZÄÜÖ][a-zäüö]* +[A-ZÄÜÖ][a-zäüö]*"  type="text" />
                     <input id="newContactMailValue" required placeholder="Email" type="email" />
                     <input id="newContactPhoneValue" required placeholder="Phone" type="tel" />
                     <div class="cont-create-contact-buttons">
@@ -350,7 +350,7 @@ function openEditContactTemp(contactName, contactMail, contactColor, contactPhon
             <div class="cont-create-contact-infos">
                 <div style="background-color: ${contactColor};" class="edit-contact-img">${bothFirstLetters}</div>
                 <form onsubmit="editContactSave('${contactName}', '${contactMail}', '${contactPhone}');return false" class="cont-create-contact-input">
-                    <input id="editContactNameValue" required value="${contactName}" placeholder="Name" type="text">
+                    <input id="editContactNameValue" required value="${contactName}"  type="name" placeholder="Vorname Nachname" pattern="[A-ZÄÜÖ][a-zäüö]* +[A-ZÄÜÖ][a-zäüö]*" >
                     <input id="editContactMailValue" required value="${contactMail}" placeholder="Email" type="email">
                     <input id="editContactPhoneValue" required value="${contactPhone}" placeholder="Phone" type="tel">
                     <div class="cont-create-contact-buttons">
